e408cdde95917831bf42cb253a2c03f375cb9626
Thiago M Rocha <kimo@kimo.io>
2014-02-06 11:05:20 -0200

    Example: VISA credit card validation

--- specs/custom_rule_spec.js
+++ specs/custom_rule_spec.js
@@ -36,6 +36,49 @@ describe("CustomRule", function(){
         message: "The value field must be a negative number."
       });
     });
+
+    describe("credit cards", function() {
+
+      describe("VISA", function() {
+
+
+        it("with a non-valid VISA number", function() {
+          var visaRule = new window.valkyr.CustomRule({
+            name: "VISA",
+            message: "The %s field does not have a valid VISA credit-card number.",
+            inherits: "credit-card",
+            validator: function(number) {
+              return !!number.match(/^4/);
+            }
+          });
+
+          expect(
+            visaRule.$check("CC", "5555555555554444")
+          ).toEqual({
+            isOk: false,
+            message: "The CC field does not have a valid VISA credit-card number."
+          });
+        });
+
+        it("with a valid VISA number", function() {
+          var visaRule = new window.valkyr.CustomRule({
+            name: "VISA",
+            message: "The %s field does not have a valid VISA credit-card number.",
+            inherits: "credit-card",
+            validator: function(number) {
+              return !!number.match(/^4/);
+            }
+          });
+
+          expect(
+            visaRule.$check("CC", "4111111111111111")
+          ).toEqual({
+            isOk: true
+          });
+        });
+
+      });
+    });
   });
 
 });
